86c51eada61e55c44d464527f862994ee36f6e1a
malinek <malin.ekelund@gmail.com>
2018-12-07 14:08:01 +0100

    more work on modal, fixed problem

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,15 @@
+//********  MODAL  *********//
+
+/***** FIRST WORK ITEM *****/
+
 const portfolioContainer = document.querySelector('.work-item')
 
 portfolioContainer.addEventListener('click', e => {
   //console.log(e)
-  e.preventDefault()
+  //e.preventDefault()
 
   const modalToggle = e.target.closest('.portfolio-link')
-    //console.log(modalToggle)
+    // console.log(modalToggle)
     if (! modalToggle) return
 
   const modal = modalToggle.parentNode.nextElementSibling
@@ -37,5 +41,93 @@ portfolioContainer.addEventListener('click', e => {
   })
 
   modalOpen()
+
+})
+
+/***** SECOND WORK ITEM *****/
+
+const portfolioContainer2 =
+document.querySelector('.work-item2')
+
+portfolioContainer2.addEventListener('click', e => {
+  //console.log(e)
+
+  const modalToggle = e.target.closest('.portfolio-link')
+  // console.log(modalToggle)
+  if (! modalToggle) return
+
+const modal = modalToggle.parentNode.nextElementSibling
+const closeButton = modal.querySelector('.modal-close')
+
+const modalOpen = _ => {
+  modal.classList.add('is-open')
+  modal.style.animation = 'modalIn 500ms forwards'
+  //document.body.style.overflowY = 'hidden'
+}
+
+const modalClose = _ => {
+  modal.classList.remove('is-open')
+  modal.removeEventListener('animationend', modalClose)
+}
+
+closeButton.addEventListener('click', _ => {
+  modal.style.animation = 'modalOut 500ms forwards'
+  modal.addEventListener('animationend', modalClose)
+  //document.body.style.overflowY = 'scroll'
+})
+
+document.addEventListener('keydown', e => {
+  if ( e.keyCode === 27 ) {
+    modal.style.animation = 'modalOut 500ms forwards'
+    modal.addEventListener('animationend', modalClose)
+    document.body.style.overflowY = 'scroll'
+  }
+})
+
+modalOpen()
+
+})
+
+/***** THIRD WORK ITEM *****/
+
+const portfolioContainer3 =
+document.querySelector('.work-item3')
+
+portfolioContainer3.addEventListener('click', e => {
+  //console.log(e)
+
+  const modalToggle = e.target.closest('.portfolio-link')
+  // console.log(modalToggle)
+  if (! modalToggle) return
+
+const modal = modalToggle.parentNode.nextElementSibling
+const closeButton = modal.querySelector('.modal-close')
+
+const modalOpen = _ => {
+  modal.classList.add('is-open')
+  modal.style.animation = 'modalIn 500ms forwards'
+  //document.body.style.overflowY = 'hidden'
+}
+
+const modalClose = _ => {
+  modal.classList.remove('is-open')
+  modal.removeEventListener('animationend', modalClose)
+}
+
+closeButton.addEventListener('click', _ => {
+  modal.style.animation = 'modalOut 500ms forwards'
+  modal.addEventListener('animationend', modalClose)
+  //document.body.style.overflowY = 'scroll'
+})
+
+document.addEventListener('keydown', e => {
+  if ( e.keyCode === 27 ) {
+    modal.style.animation = 'modalOut 500ms forwards'
+    modal.addEventListener('animationend', modalClose)
+    document.body.style.overflowY = 'scroll'
+  }
+})
+
+modalOpen()
 
 })

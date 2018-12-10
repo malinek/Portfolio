fb26416a690d6cbdc73874e74f3aaf3f8906712c
malinek <malin.ekelund@gmail.com>
2018-12-10 18:11:43 +0100

    edit modals and some cleaning up

--- js/modal.js
+++ js/modal.js
@@ -135,13 +135,15 @@ modalOpen()
 /***** MODAL PRIVACY POLICY *****/
 
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementById('modal-footer');
 
 // Get the button that opens the modal
 var btn = document.getElementById("button-privacy-policy");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var closeTop = document.getElementsByClassName("close")[0];
+
+var closeBottom = document.getElementsByClassName("close-modal-bottom")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
@@ -149,13 +151,10 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeTop.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+closeBottom.onclick = function() {
+  modal.style.display = "none";
 }

a18626f7c1c9b0b6e89e87fdbbaa55e2c009b8e7
malinek <malin.ekelund@gmail.com>
2018-12-12 11:41:56 +0100

    add smooth scrolling

--- js/navbar.js
+++ js/navbar.js
@@ -6,7 +6,35 @@ $(document).ready(function(){
     $('.main-nav').toggleClass('main-nav--open', 500);
     $(this).toggleClass('hamburger--open');
   });
- 
+
+
+  // Smooth scrolling
+  var $root = $('html, body');
+  $('.main-nav a').click(function() {
+    var href = $.attr(this, 'href');
+    if (href != undefined && href != '#') {
+    $root.animate({
+      scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+    }
+    return false;
+  });
+
+  var $root = $('html, body');
+  $('.button-bottom a').click(function() {
+    var href = $.attr(this, 'href');
+    if (href != undefined && href != '#') {
+    $root.animate({
+      scrollTop: $(href).offset().top
+    }, 500, function () {
+        window.location.hash = href;
+    });
+    }
+    return false;
+  });
+
 }); // document ready function
 
 
